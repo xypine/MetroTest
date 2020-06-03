b638e99d88c90247cea4326170603f3843cea9a3
Elias Eskelinen <elias.eskelinen@protonmail.com>
2020-06-03 12:44:59 +0300

    init

---
+++ src/metro/Demo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metro;
+
+/**
+ *
+ * @author Elias Eskelinen <elias.eskelinen@protonmail.com>
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frame.main(args);
+    }
+    
+}
